1415ace56e3bf6496ce37165a53424eb5a66da71
Denay2468 <denayaung13@gmail.com>
2026-04-07 13:40:57 +0630

    create input function and code

--- mergesort.cpp
+++ mergesort.cpp
@@ -5,5 +5,16 @@ int arr[20] , B[20];
 
 int n;
 
-
+void input()
+{
+    while (true
+    ) {
+        cout << "Enter the number of elements (1-20): ";
+        cin >> n;
+        if (n <= 20) {
+            break;
+        }
+        cout << "Invalid input. Please enter a number between 1 and 20." << endl;
+    }
+}
 
